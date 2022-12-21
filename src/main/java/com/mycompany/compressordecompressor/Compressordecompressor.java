2e21d6987c8b544047feac99cfce5fdc8d2562e3
Angad <anilterapalli@gmail.com>
2022-12-21 20:38:03 +0530

    MinorProject

---
+++ src/main/java/com/mycompany/compressordecompressor/Compressordecompressor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compressordecompressor;
+
+/**
+ *
+ * @author 91809
+ */
+public class Compressordecompressor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
